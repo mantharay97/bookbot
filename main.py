2d6c341108280e33b898aba7340f1343b1ea4c30
mantharay97 <mantharay97@gmail.com>
2025-10-19 12:49:55 -0500

    Print words

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def count_words(text):
+    return len(text.split())
+
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
 
 if __name__ == "__main__":
     main()
